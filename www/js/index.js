23516a552865b5a64488d215858e70bfc02579dd
Beauty <beautymalingamoyo@gmail.com>
2026-02-23 16:31:30 +0200

    added code to create database and tables

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,29 @@
-/**
-    Licensed to the Apache Software Foundation (ASF) under one
-    or more contributor license agreements.  See the NOTICE file
-    distributed with this work for additional information
-    regarding copyright ownership.  The ASF licenses this file
-    to you under the Apache License, Version 2.0 (the
-    "License"); you may not use this file except in compliance
-    with the License.  You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing,
-    software distributed under the License is distributed on an
-    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-    KIND, either express or implied.  See the License for the
-    specific language governing permissions and limitations
-    under the License.
-*/
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    // This line creates the physical .db file if it's the first time the app is opened.
+    // Otherwise, it simply establishes a connection to the existing file.
+    // Open the database
+    db = window.sqlitePlugin.openDatabase({
+        name: 'citization.db',
+        location: 'default', // Essential for Android/iOS
+        androidDatabaseProvider: 'system' // Recommended for modern Android
+    });
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    // If the tables already exist, does nothing, and moves to the "Success" callback
+    // Initialize your tables
+    db.transaction(function(tx) {
+        // user Management Tables
+        tx.executeSql('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT, ' + 
+            'username TEXT NOT NULL, password TEXT NOT NULL, role TEXT NOT NULL, date_registered DATETIME, ' +
+            'date_updated DATETIME DEFAULT CURRENT_TIMESTAMP');
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS citizen (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+            'user_id INTEGER NOT NULL, spouse_id INTEGER, firstname TEXT NOT NULL, lastname TEXT NOT NULL, ' +
+            'home_address TEXT NOT NULL, contact INTEGER, DOB DATETIME NOT NULL, village TEXT NOT NULL, ' +
+            'district TEXT NOT NULL, treditional_authority TEXT, date_registered DATETIME NOT NULL, ' +
+            'date_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)');
+    }, function(error) {
+        alert("Initialization Error! : " + error.message);
+    });
 }
